065f3e1b88f0f89c59ea6856ef39d35e2710001c
Quyen Trinh <vanquyen1994@gmail.com>
2023-05-17 14:58:04 +0700

    release 1.0.4

--- Sources/MyPackage/MyPackage.swift
+++ Sources/MyPackage/MyPackage.swift
@@ -1,10 +1,14 @@
 public struct MyPackage {
     public private(set) var text = "Hello, World!"
-    private let version = "v1.0.3"
+    private let version = "v1.0.4"
     public init() {
     }
     
     public func getVersion() -> String {
         return version
+    }
+    
+    public func sayHello() {
+        print(text)
     }
 }
